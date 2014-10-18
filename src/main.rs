5892c1cf4e022f39162446c0d52f87108bcf3790
Mark Schmale <masch@masch.it>
2014-10-18 15:41:43 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,21 @@ use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
 use std::io::BufferedReader;
 
-struct Configuration {
-    port: u16,
-    hostname: &'static str
-}
-
 fn handle_request(host: &str, mut client: TcpStream) {
     println!("handling request for {}", host);
     client.write(b"127.0.0.1:9001\n");
+}
+
+fn handle_new_client(mut stream: TcpStream) {
+    let mut reader = BufferedReader::new(stream);
+    let req = reader.read_line();
+    stream = reader.unwrap();
+    match req {
+        Ok(host) => handle_request(host.as_slice(), stream),
+        Err(msg) => {
+            stream.write(b"error");
+        }
+    };
 }
 
 fn main() {
@@ -27,27 +34,14 @@ fn main() {
 
     let host = config.get("hostname").as_slice();
 
-    println!("bind to {}:{}", host, port);
-
     let socket = TcpListener::bind(host, port);
-
     let mut acceptor = socket.listen();
-
-    fn handle_new_client(mut stream: TcpStream) {
-        let mut reader = BufferedReader::new(stream);
-        let req = reader.read_line();
-        stream = reader.unwrap();
-        match req {
-            Ok(host) => handle_request(host.as_slice(), stream),
-            Err(msg) => {
-                stream.write(b"error");
-            }
-        };
-    }
 
     for stream in acceptor.incoming() {
         match stream {
-            Err(e) => { /* connection failed */ }
+            Err(e) => {
+                println!("connection failed: {}", e);
+            },
             Ok(stream) => spawn(proc() {
                 // connection succeeded
                 handle_new_client(stream)
