b13980a9e3eadd7a97388626bfe5f9a7fd1b2c79
Mark Schmale <masch@masch.it>
2014-10-18 16:49:53 +0200

    using asynchronus backends works! yay

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,50 @@ use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
 use std::io::BufferedReader;
 
-fn handle_request(host: &str, mut client: TcpStream) {
-    println!("handling request for {}", host);
-    client.write(b"127.0.0.1:9001\n");
+use std::comm::{Sender, Receiver};
+
+use std::io::Timer;
+use std::time::Duration;
+
+fn start_random_backend(rx: Receiver<(String, Sender<Option<String>>)>) {
+    loop {
+        let (host, answer) = rx.recv();
+        let mut timer = Timer::new().unwrap();
+        timer.sleep(Duration::milliseconds(1000));
+        answer.send(Some(host + " found"));
+    }
 }
 
-fn handle_new_client(mut stream: TcpStream) {
+fn handle_new_client(mut stream: TcpStream, tx: Sender<(String, Sender<Option<String>>)>) {
     let mut reader = BufferedReader::new(stream);
     let req = reader.read_line();
     stream = reader.unwrap();
-    match req {
-        Ok(host) => handle_request(host.as_slice(), stream),
+    let result = match req {
+        Ok(host) => {
+            let thost = host.as_slice().trim().to_string();
+            let (mytx, myrx) = channel();
+            tx.send((thost, mytx));
+            let answer = myrx.recv();
+            println!("answer: {}", answer);
+            match answer {
+                Some(address) => {
+                    stream.write(address.as_bytes())
+                },
+                None => {
+                    stream.write(b"404")
+                }
+            }
+
+        },
         Err(msg) => {
-            stream.write(b"error");
+            stream.write(format!("error: {}", msg).as_bytes())
         }
+    };
+    match result {
+        Err(msg) => {
+            println!("ERROR: {}", msg);
+        }
+        _ => ()
     };
 }
 
@@ -34,17 +64,25 @@ fn main() {
 
     let host = config.get("hostname").as_slice();
 
+    // start backend
+    let (tx, rx) = channel();
+
+    spawn(proc() {
+        start_random_backend(rx);
+    });
+
     let socket = TcpListener::bind(host, port);
     let mut acceptor = socket.listen();
 
     for stream in acceptor.incoming() {
+        let ptx = tx.clone();
         match stream {
             Err(e) => {
                 println!("connection failed: {}", e);
             },
             Ok(stream) => spawn(proc() {
                 // connection succeeded
-                handle_new_client(stream)
+                handle_new_client(stream, ptx)
             })
         }
     }
